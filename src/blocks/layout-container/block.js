d3c0aac23527418a9eda3cff76e558ebfcef86b5
zerel <hankzang@gmail.com>
2019-08-19 13:18:31 +1200

    update color layout container base on slider-item container for better user experience

--- src/blocks/layout-container/block.js
+++ src/blocks/layout-container/block.js
@@ -3,22 +3,171 @@
  * @Author: Hank
  * @Date: 2019-05-24 10:49:35
  * @Last Modified by: Hank
- * @Last Modified time: 2019-06-07 10:57:49
+ * @Last Modified time: 2019-08-19 13:13:40
  */
 
 /* eslint-disable prefer-const */
 /* eslint-disable no-console */
 
-//  Import CSS.
-import './style.scss';
-import './editor.scss';
-import Inspector from './components/Inspector';
-const { Button, Modal, Placeholder } = wp.components;
-const { RichText, InnerBlocks } = wp.editor;
-const { Component, Fragment } = wp.element;
+// Import block dependencies and components
+import classnames from 'classnames';
+import Inspector from './components/inspector';
+import Container from './components/container';
+import deprecated from './deprecated';
 
+// Import CSS
+import './styles/style.scss';
+import './styles/editor.scss';
+
+// Components
 const { __ } = wp.i18n;
+
+// Extend component
+const { Component } = wp.element;
+
+// Register block
 const { registerBlockType } = wp.blocks;
+
+// Register editor components
+const {
+	AlignmentToolbar,
+	BlockControls,
+	BlockAlignmentToolbar,
+	MediaUpload,
+	RichText,
+	InnerBlocks,
+} = wp.editor;
+
+// Register components
+const {
+	Button,
+	withFallbackStyles,
+	IconButton,
+	Dashicon,
+	withState,
+	Toolbar,
+} = wp.components;
+
+const blockAttributes = {
+	containerPaddingTop: {
+		type: 'number',
+	},
+	containerPaddingRight: {
+		type: 'number',
+	},
+	containerPaddingBottom: {
+		type: 'number',
+	},
+	containerPaddingLeft: {
+		type: 'number',
+	},
+	containerMarginTop: {
+		type: 'number',
+	},
+	containerMarginBottom: {
+		type: 'number',
+	},
+	containerWidth: {
+		type: 'string',
+	},
+	containerMaxWidth: {
+		type: 'number',
+		default: 1600,
+	},
+	containerBackgroundColor: {
+		type: 'string',
+	},
+	containerImgURL: {
+		type: 'string',
+		source: 'attribute',
+		attribute: 'src',
+		selector: 'img',
+	},
+	containerImgID: {
+		type: 'number',
+	},
+	containerImgAlt: {
+		type: 'string',
+		source: 'attribute',
+		attribute: 'alt',
+		selector: 'img',
+	},
+	containerDimRatio: {
+		type: 'number',
+		default: 100,
+	},
+};
+
+class edit extends Component {
+	render() {
+		// Setup the attributes
+		const {
+			attributes: {
+				containerPaddingTop,
+				containerPaddingRight,
+				containerPaddingBottom,
+				containerPaddingLeft,
+				containerMarginTop,
+				containerMarginBottom,
+				containerWidth,
+				containerMaxWidth,
+				containerBackgroundColor,
+				containerImgURL,
+				containerImgID,
+				containerImgAlt,
+				containerDimRatio,
+			},
+			attributes,
+			isSelected,
+			editable,
+			className,
+			setAttributes,
+		} = this.props;
+
+		const onSelectImage = img => {
+			setAttributes( {
+				containerImgID: img.id,
+				containerImgURL: img.url,
+				containerImgAlt: img.alt,
+			} );
+		};
+
+		return [
+			// Show the block controls on focus
+			<Inspector {...{ setAttributes, ...this.props }} />,
+
+			// Show the container markup in the editor
+			<Container {...this.props}>
+				<div className="fl-container-inside">
+					{containerImgURL && !! containerImgURL.length && (
+						<div className="fl-container-image-wrap">
+							<img
+								className={classnames(
+									'fl-container-image',
+									dimRatioToClass( containerDimRatio ),
+									{
+										'has-background-dim': 0 !== containerDimRatio,
+									}
+								)}
+								src={containerImgURL}
+								alt={containerImgAlt}
+							/>
+						</div>
+					)}
+
+					<div
+						className="fl-container-content"
+						style={{
+							maxWidth: `${ containerMaxWidth }px`,
+						}}
+					>
+						<InnerBlocks />
+					</div>
+				</div>
+			</Container>,
+		];
+	}
+}
 
 registerBlockType( 'futurelab/block-layout-container', {
 	title: __( 'fl Color Layout Container' ),
@@ -28,85 +177,75 @@ registerBlockType( 'futurelab/block-layout-container', {
 		src: 'wordpress-alt',
 	},
 	category: 'fl-Blocks',
-	keywords: [ __( 'futurelab slider block' ) ],
+	keywords: [ __( 'futurelab Layout block' ) ],
 
-	attributes: {
-		backgroundColor: {
-			source: 'attribute',
-			type: 'string',
-			attribute: 'data-bgc',
-			selector: '.fl-layout-container',
-			default: '',
-		},
-		imageUrl: {
-			source: 'attribute',
-			type: 'string',
-			attribute: 'data-imageUrl',
-			selector: '.fl-layout-container',
-			default: '',
-		},
-	},
-	edit: class extends Component {
-		constructor( props ) {
-			super( ...arguments );
-			this.props = props;
-			this.state = {};
-		}
-		// component finish loading
-		componentDidMount() {
-			const { attributes, setAttributes } = this.props;
-		}
+	attributes: blockAttributes,
 
-		render() {
-			const { attributes, setAttributes, className } = this.props;
-			const { backgroundColor, imageUrl } = attributes;
-			console.log( 'backgroundColor', backgroundColor );
-
-			const TEMPLATE = [ [ 'core/column', {}, [] ] ];
-
-			return [
-				<Inspector {...{ setAttributes, ...this.props }} />,,
-				<div
-					className={className}
-					style={{
-						backgroundColor: backgroundColor,
-						backgroundImage: `url(${ imageUrl })`,
-						backgroundSize: 'cover',
-						backgroundRepeat: 'no-repeat',
-						backgroundPosition: '50% 60%',
-						minHeight: '200px',
-					}}
-					data-bgc={backgroundColor}
-					data-imageUrl={imageUrl}
-				>
-					{/* <InnerBlocks template={TEMPLATE} isFocused /> */}
-					<InnerBlocks template={TEMPLATE} />
-				</div>,
-			];
+	getEditWrapperProps( { containerWidth } ) {
+		if (
+			'left' === containerWidth ||
+			'right' === containerWidth ||
+			'full' === containerWidth
+		) {
+			return { 'data-align': containerWidth };
 		}
 	},
 
-	save: ( { attributes, setAttributes } ) => {
-		const { backgroundColor, imageUrl } = attributes;
-		console.log( 'backgroundColor', backgroundColor );
+	edit: edit,
+
+	// Save the attributes and markup
+	save: function( props ) {
+		// Setup the attributes
+		const {
+			containerPaddingTop,
+			containerPaddingRight,
+			containerPaddingBottom,
+			containerPaddingLeft,
+			containerMarginTop,
+			containerMarginBottom,
+			containerWidth,
+			containerMaxWidth,
+			containerBackgroundColor,
+			containerImgURL,
+			containerImgID,
+			containerImgAlt,
+			containerDimRatio,
+		} = props.attributes;
+
+		// Save the block markup for the front end
 		return (
-			<div
-				className={'fl-layout-container'}
-				style={{
-					backgroundColor: backgroundColor,
-					backgroundImage: `url(${ imageUrl })`,
-					backgroundSize: 'cover',
-					backgroundRepeat: 'no-repeat',
-					backgroundPosition: '50% 60%',
-					minHeight: '200px',
-				}}
-				data-bgc={backgroundColor}
-				data-imageUrl={imageUrl}
-			>
-				<InnerBlocks.Content />
-			</div>
+			<Container {...props}>
+				<div className="fl-container-inside">
+					{containerImgURL && !! containerImgURL.length && (
+						<div className="fl-container-image-wrap">
+							<img
+								className={classnames(
+									'fl-container-image',
+									dimRatioToClass( containerDimRatio ),
+									{
+										'has-background-dim': 0 !== containerDimRatio,
+									}
+								)}
+								src={containerImgURL}
+								alt={containerImgAlt}
+							/>
+						</div>
+					)}
+
+					<div
+						className="fl-container-content"
+						style={{
+							maxWidth: `${ containerMaxWidth }px`,
+						}}
+					>
+						<InnerBlocks.Content />
+					</div>
+				</div>
+			</Container>
 		);
 	},
+
+	deprecated: deprecated,
 	useOnce: false,
 	supports: {
 		align: true,
@@ -114,3 +253,13 @@ registerBlockType( 'futurelab/block-layout-container', {
 		customClassName: false,
 	},
 } );
+
+function dimRatioToClass( ratio ) {
+	return 0 === ratio || 50 === ratio ?
+		null :
+		'has-background-dim-' + 10 * Math.round( ratio / 10 );
+}
+
+function backgroundImageStyles( url ) {
+	return url ? { backgroundImage: `url(${ url })` } : undefined;
+}
