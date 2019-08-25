b68ee7453e83cd5d8a366aea547133fb6ed9127d
zerel <hankzang@gmail.com>
2019-08-26 10:24:35 +1200

    add accordion block

--- src/blocks/accordion/components/save.js
+++ src/blocks/accordion/components/save.js
@@ -2,7 +2,7 @@
  * @Author: Hank
  * @Date: 2019-08-23 14:07:37
  * @Last Modified by: Hank
- * @Last Modified time: 2019-08-23 14:50:21
+ * @Last Modified time: 2019-08-26 10:12:11
  */
 // Import block dependencies and components
 import Accordion from './accordion';
@@ -19,16 +19,24 @@ export default class Save extends Component {
 	}
 
 	render() {
+		const { accordionOpen } = this.props.attributes;
+
+		console.log( 'accordionOpen', accordionOpen );
 		return (
 			<Accordion {...this.props}>
-				<details open={this.props.attributes.accordionOpen}>
-					<summary className="fl-accordion-title">
-						<RichText.Content value={this.props.attributes.accordionTitle} />
-					</summary>
-					<div className="fl-accordion-text">
-						<InnerBlocks.Content />
-					</div>
-				</details>
+				<button
+					className={`accordion fl-accordion-title ${
+						accordionOpen ? 'active' : ''
+					}`}
+				>
+					<RichText.Content value={this.props.attributes.accordionTitle} />
+				</button>
+				<div
+					className="panel"
+					style={`display: ${ accordionOpen ? 'block' : 'none' }`}
+				>
+					<InnerBlocks.Content />
+				</div>
 			</Accordion>
 		);
 	}
